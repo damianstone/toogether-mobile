73e783c343663cdc6f75561480476f457894bd86
AlfredoArizmende <k.arizmende@gmail.com>
2023-03-21 00:33:11 -0600

    Fix expo-app-loading no comments

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ import {
   ActionSheetProvider,
   connectActionSheet,
 } from '@expo/react-native-action-sheet';
-// import AppLoading from 'expo-app-loading';
 import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 
@@ -14,46 +13,13 @@ import { ContextProvider } from './context/ContextProvider';
 import store from './store/store';
 
 SplashScreen.preventAutoHideAsync();
-// const fetchFonts = async () => {
-
-//   const loadedFonts = await Font.loadAsync({
-//     'poppins-regular': require('./assets/fonts/Poppins-Regular.ttf'),
-//     'poppins-bold': require('./assets/fonts/Poppins-Bold.ttf'),
-//   });
-//   return loadedFonts;
-// };
 
 const App = () => {
-  //   const [fontLoaded, setFontLoaded] = useState(false);
-
-  //   const stopSplash = async () => {
-  //     try {
-  //       await SplashScreen.hideAsync();
-  //       setFontLoaded(true);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
-
-  //   if (!fontLoaded) {
-  //     return (
-  //       <AppLoading
-  //         startAsync={fetchFonts}
-  //         onError={(err) => console.warn(err)}
-  //         onFinish={stopSplash}
-  //         autoHideSplash
-  //       />
-  //     );
-  //   }
-
-  // -------------------------------------------
-  // -------------------------------------------
   const [appIsReady, setAppIsReady] = useState(false);
 
   useEffect(() => {
     const prepare = async () => {
       try {
-        // Pre-load fonts, make any API calls you need to do here
         await Font.loadAsync({
           'poppins-regular': require('./assets/fonts/Poppins-Regular.ttf'),
           'poppins-bold': require('./assets/fonts/Poppins-Bold.ttf'),
@@ -61,7 +27,6 @@ const App = () => {
       } catch (e) {
         console.warn(e);
       } finally {
-        // Tell the application to render
         setAppIsReady(true);
       }
     }
@@ -77,19 +42,7 @@ const App = () => {
 
   if (!appIsReady) {
     return null;
-    // return (
-    //   <AppLoading
-    //     // startAsync={fetchFonts}
-    //     // onError={(err) => console.warn(err)}
-    //     onFinish={onLayoutRootView}
-    //     autoHideSplash
-    //   />
-    // );
   }
-
-
-  // -------------------------------------------
-  // -------------------------------------------
 
   return (
     <Provider store={store}>
