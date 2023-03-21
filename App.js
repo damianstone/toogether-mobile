30da3d914373daf84d987a9c51044ea3ab184b3f
AlfredoArizmende <k.arizmende@gmail.com>
2023-03-21 00:18:01 -0600

    Fix expo-app-loading

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect, useCallback } from 'react';
+import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import {
   ActionSheetProvider,
   connectActionSheet,
 } from '@expo/react-native-action-sheet';
-import AppLoading from 'expo-app-loading';
+// import AppLoading from 'expo-app-loading';
 import * as Font from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 
@@ -13,42 +14,88 @@ import { ContextProvider } from './context/ContextProvider';
 import store from './store/store';
 
 SplashScreen.preventAutoHideAsync();
+// const fetchFonts = async () => {
 
-const fetchFonts = async () => {
-  const loadedFonts = await Font.loadAsync({
-    'poppins-regular': require('./assets/fonts/Poppins-Regular.ttf'),
-    'poppins-bold': require('./assets/fonts/Poppins-Bold.ttf'),
-  });
-  return loadedFonts;
-};
+//   const loadedFonts = await Font.loadAsync({
+//     'poppins-regular': require('./assets/fonts/Poppins-Regular.ttf'),
+//     'poppins-bold': require('./assets/fonts/Poppins-Bold.ttf'),
+//   });
+//   return loadedFonts;
+// };
 
 const App = () => {
-  const [fontLoaded, setFontLoaded] = useState(false);
+  //   const [fontLoaded, setFontLoaded] = useState(false);
 
-  const stopSplash = async () => {
-    try {
-      await SplashScreen.hideAsync();
-      setFontLoaded(true);
-    } catch (error) {
-      console.log(error);
+  //   const stopSplash = async () => {
+  //     try {
+  //       await SplashScreen.hideAsync();
+  //       setFontLoaded(true);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+
+  //   if (!fontLoaded) {
+  //     return (
+  //       <AppLoading
+  //         startAsync={fetchFonts}
+  //         onError={(err) => console.warn(err)}
+  //         onFinish={stopSplash}
+  //         autoHideSplash
+  //       />
+  //     );
+  //   }
+
+  // -------------------------------------------
+  // -------------------------------------------
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    const prepare = async () => {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync({
+          'poppins-regular': require('./assets/fonts/Poppins-Regular.ttf'),
+          'poppins-bold': require('./assets/fonts/Poppins-Bold.ttf'),
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
     }
-  };
 
-  if (!fontLoaded) {
-    return (
-      <AppLoading
-        startAsync={fetchFonts}
-        onError={(err) => console.warn(err)}
-        onFinish={stopSplash}
-        autoHideSplash
-      />
-    );
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+    // return (
+    //   <AppLoading
+    //     // startAsync={fetchFonts}
+    //     // onError={(err) => console.warn(err)}
+    //     onFinish={onLayoutRootView}
+    //     autoHideSplash
+    //   />
+    // );
   }
+
+
+  // -------------------------------------------
+  // -------------------------------------------
 
   return (
     <Provider store={store}>
       <ActionSheetProvider>
         <ContextProvider>
+          <View onLayout={onLayoutRootView} />
           <ToogetherNavigation theme="dark" />
         </ContextProvider>
       </ActionSheetProvider>
