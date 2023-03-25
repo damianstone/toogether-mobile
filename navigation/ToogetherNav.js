44fbf210fa261070c5d6d889162294a073a2b930
AlfredoArizmende <k.arizmende@gmail.com>
2023-03-25 12:34:07 -0600

    Final Token Refresh Implementation

--- navigation/ToogetherNav.js
+++ navigation/ToogetherNav.js
@@ -1,8 +1,7 @@
 import React, { useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { NavigationActions } from 'react-navigation'; // navigate outside of the navigator
-
+import { NavigationActions } from 'react-navigation';
 import TooNavigation from './Navigation';
 import { updateToken, logout } from '../store/actions/user';
 
@@ -14,29 +13,22 @@ const ToogetherNav = () => {
 
   const checkToken = async () => {
     const userData = JSON.parse(await AsyncStorage.getItem('@userData'));
-
-    if (userData) {
-      console.log(userData);
+    
+    if (userData && userData.has_account) {
       const tokenDecoded = jwt_decode(userData.token);
       const refreshTokenDecoded = jwt_decode(userData.refresh_token);
       const isTokenExpired = tokenDecoded.exp < (Date.now() / 1000);
       const isRefreshTokenExpired = refreshTokenDecoded.exp < (Date.now() / 1000);
 
-      console.log('Change screen');
-      console.log(tokenDecoded.exp, isTokenExpired, (Date.now() / 1000));
-      console.log(refreshTokenDecoded.exp, isRefreshTokenExpired, (Date.now() / 1000));
-
       if (isTokenExpired && !isRefreshTokenExpired) {
-        console.log('refresh');
         dispatch(updateToken());
       }
 
       if (isTokenExpired && isRefreshTokenExpired) {
-        console.log('expired');
         dispatch(logout());
         navRef.current.dispatch(
           NavigationActions.navigate({
-            routeName: 'AuthStart',
+            routeName: 'AuthStart'
           })
         )
       }
