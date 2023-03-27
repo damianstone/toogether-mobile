87ade9c04f45014a6ec514e91ae8a7e361f2f2fd
Damian Stone <damianstonedev@gmail.com>
2023-03-27 17:38:09 +0100

    real-time chat next update text

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -212,8 +212,7 @@ const ChatScreen = (props) => {
           />
           <TouchableOpacity
             onPress={() => onOpenActionSheet(matchedProfile, item.id)}
-            style={styles.cardContainer}
-          >
+            style={styles.cardContainer}>
             <Text style={styles.instagramText}>
               {matchedProfile.instagram
                 ? `@ ${matchedProfile.instagram}`
@@ -222,8 +221,7 @@ const ChatScreen = (props) => {
             {matchedProfile.instagram ? (
               <TouchableOpacity
                 onPress={() => handleInstagram(matchedProfile.instagram)}
-                style={styles.sendButtonContainer}
-              >
+                style={styles.sendButtonContainer}>
                 <Image
                   source={require('../assets/images/send-button.png')}
                   style={styles.img}
@@ -246,8 +244,7 @@ const ChatScreen = (props) => {
           width: '100%',
           height: '100%',
           textAlign: 'center',
-        }}
-      >
+        }}>
         <View style={{ width: 200, height: 200 }}>
           <Image
             source={require('../assets/images/no-chats.png')}
@@ -278,6 +275,17 @@ const ChatScreen = (props) => {
         renderItem={renderMatch}
         ListEmptyComponent={renderEmptyMatches}
       />
+      <View
+        style={{
+          flex: 1,
+          justifyContent: 'flex-end',
+          marginBottom: 36,
+          alignItems: 'center',
+        }}>
+        <Text style={{ color: Colors.white }}>
+          Real-time chat in the next update ;)
+        </Text>
+      </View>
     </View>
   );
 };
