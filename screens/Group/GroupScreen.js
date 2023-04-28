9f79c45e0e9d97b84db7a46d71a8c371cd8ad647
Damian Stone <damianstonedev@gmail.com>
2023-04-29 00:23:09 +0100

    check for empty object and change message

--- screens/Group/GroupScreen.js
+++ screens/Group/GroupScreen.js
@@ -324,9 +324,9 @@ const GroupScreen = (props) => {
               backgroundColor={Colors.orange}
             />
           )}
-          {groupContext.members.length <= 1 && (
+          {groupContext?.members.length <= 1 && (
             <Text style={{ fontSize: 10, color: Colors.placeholder }}>
-              Group incognito until 2 members join
+              Group incognito until 1 more members joins
             </Text>
           )}
         </View>
