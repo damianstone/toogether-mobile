fc6d23c5aeb16dcd97f0dd4af0a92c14eb8cb61e
MartinOravecSvK <oravec.martin01@gmail.com>
2023-05-16 19:41:58 +0100

    minor changes

--- components/UI/Avatar.js
+++ components/UI/Avatar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { useSelector } from 'react-redux';
 import Colors from '../../constants/Colors';
 import { getNameInitials, getImage } from '../../utils/getMethods';
